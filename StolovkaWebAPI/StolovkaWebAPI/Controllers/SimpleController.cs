c45bee454b5798be6e508bbb021666ca8c9934d6
SaveliyLipaev <sava.lipaev@mail.ru>
2019-10-08 18:18:31 +0300

    Added asp.net core api + xunit + stylecop

---
+++ StolovkaWebAPI/StolovkaWebAPI/Controllers/SimpleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace StolovkaWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SimpleController : ControllerBase
+    {
+        [HttpGet]
+        public int Get()
+        {
+            return 1;
+        }
+    }
+}

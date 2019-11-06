ec90194518c6bcbf037e527fa5ef3cb15833cca8
SaveliyLipaev <sava.lipaev@mail.ru>
2019-11-06 14:45:45 +0300

    working authorization through vk

---
+++ XamarinMobileApp/XamarinMobileApp/Views/LoginPage.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinMobileApp;
+
+namespace XamarinMobileApp.Views
+{
+    public class LoginPage : ContentPage
+    {
+        readonly Label _hintLabel;
+        readonly List<Button> _loginButtons = new List<Button>();
+        bool _isAuthenticated;
+
+        public LoginPage()
+        {
+            Title = "Stolovka Login";
+
+            _hintLabel = new Label
+            {
+                HorizontalTextAlignment = TextAlignment.Center,
+                Text = "Unauthenticated"
+            };
+
+            var stackLayout = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = { _hintLabel }
+            };
+
+            var providers = new[] { "VK" };
+            foreach (var provider in providers)
+            {
+                var loginButton = new Button
+                {
+                    HorizontalOptions = LayoutOptions.Center,
+                    Text = $"Login {provider}",
+                    AutomationId = provider
+                };
+
+                loginButton.Clicked += LoginButtonOnClicked;
+
+                _loginButtons.Add(loginButton);
+                stackLayout.Children.Add(loginButton);
+            }
+
+            Content = stackLayout;
+        }
+
+        async void LoginButtonOnClicked(object sender, EventArgs e)
+        {
+            if (_isAuthenticated)
+            {
+                _hintLabel.Text = "Unauthenticated";
+
+                var senderBtn = sender as Button;
+                if (senderBtn == null) return;
+
+                Logout(senderBtn.AutomationId);
+
+                _isAuthenticated = false;
+                foreach (var btn in _loginButtons)
+                {
+                    btn.IsEnabled = true;
+                    btn.Text = $"Login {btn.AutomationId}";
+                }
+            }
+            else
+            {
+                var senderBtn = sender as Button;
+                if (senderBtn == null) return;
+
+                _hintLabel.Text = "Login. Please wait";
+                var loginResult = await Login(senderBtn.AutomationId);
+
+                foreach (var btn in _loginButtons.Where(b => b != senderBtn))
+                    btn.IsEnabled = false;
+
+                switch (loginResult.LoginState)
+                {
+                    case LoginState.Canceled:
+                        _hintLabel.Text = "Canceled";
+                        foreach (var btn in _loginButtons.Where(b => b != senderBtn))
+                            btn.IsEnabled = true;
+                        break;
+                    case LoginState.Success:
+                        _hintLabel.Text = $"Hi {loginResult.FirstName}! Your email is {loginResult.Email}";
+                        senderBtn.Text = $"Logout {senderBtn.AutomationId}";
+
+                        _isAuthenticated = true;
+                        break;
+                    default:
+                        _hintLabel.Text = "Failed: " + loginResult.ErrorString;
+                        foreach (var btn in _loginButtons.Where(b => b != senderBtn))
+                            btn.IsEnabled = true;
+                        break;
+                }
+            }
+        }
+
+        Task<LoginResult> Login(string providerName)
+        {
+            switch (providerName.ToLower())
+            {
+                case "vk":
+                    return DependencyService.Get<IVkService>().Login();
+                default:
+                    throw new InvalidNavigationException();
+            }
+        }
+
+        void Logout(string providerName)
+        {
+            switch (providerName.ToLower())
+            {
+                case "vk":
+                    DependencyService.Get<IVkService>().Logout();
+                    return;
+                default:
+                    throw new InvalidNavigationException();
+            }
+        }
+    }
+}
+
+
+//using System;
+//using Xamarin.Forms;
+
+//namespace XamarinMobileApp.Views
+//{
+//    public partial class LoginPage : ContentPage
+//    {
+//        public LoginPage()
+//        {
+//            InitializeComponent();
+//        }
+
+//        async void OnLoginButtonClicked(object sender, EventArgs args)
+//        {
+//            Navigation.InsertPageBefore(new MenuMainPage(), this);
+//            await Navigation.PopAsync();
+//        }
+
+//        async void OnSignUpButtonClicked(object sender, EventArgs args)
+//        {
+//            await Navigation.PushAsync(new SignUpPage());
+//        }
+//    }
+//}

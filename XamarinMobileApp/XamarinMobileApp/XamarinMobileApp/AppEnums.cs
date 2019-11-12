d1e889a32e66b2f0f53fcd01fa4b8e26e9b12bad
SaveliyLipaev <sava.lipaev@mail.ru>
2019-11-13 02:09:37 +0300

    Add infrastructure and base classes

---
+++ XamarinMobileApp/XamarinMobileApp/XamarinMobileApp/AppEnums.cs
@@ -0,0 +1,45 @@
+namespace XamarinMobileApp
+{
+	public enum Pages {
+		HotelInfo,
+		Menu,
+		Login,
+		ServiceList,
+		ServiceOrder,
+		RestaurantList,
+		RestaurantInfo,
+		RestaurantMealList,
+		MealOrder,
+		Cart,
+		FitnessList,
+		FitnessOrder,
+		GuideCategories,
+		PlaceList,
+		PlaceInfo,
+		ChatCategories,
+		Chat,
+		Profile,
+		ProfileEdit,
+		NotificationList,
+		NotificationInfo,
+		OrderHistory,
+		OrderInfo
+	}
+
+
+	public enum NavigationMode {
+		Normal,
+		Modal,
+		RootPage,
+		Custom
+	}
+
+	public enum PageState {
+		Clean,
+		Loading,
+		Normal,
+		NoData,
+		Error,
+		NoInternet
+	}
+}

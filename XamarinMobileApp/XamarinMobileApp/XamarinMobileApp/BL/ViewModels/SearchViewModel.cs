168a8bfbc1215871207c2e07b43a916767a5258e
SaveliyLipaev <sava.lipaev@mail.ru>
2019-11-15 16:32:56 +0300

    Tests viewmodel

--- XamarinMobileApp/XamarinMobileApp/XamarinMobileApp/BL/ViewModels/SearchViewModel.cs
+++ XamarinMobileApp/XamarinMobileApp/XamarinMobileApp/BL/ViewModels/SearchViewModel.cs
@@ -1,6 +1,26 @@
-﻿namespace XamarinMobileApp.BL.ViewModels
+﻿using System.Windows.Input;
+
+namespace XamarinMobileApp.BL.ViewModels
 {
     class SearchViewModel : BaseViewModel
     {
+        private int count = 0;
+        public string text
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public ICommand but => MakeCommand(async () =>
+        {
+            count++;
+            text = count.ToString();
+        });
+
+        public ICommand button => MakeCommand(async () =>
+        {
+            count++;
+            text = count.ToString();
+        });
     }
 }
